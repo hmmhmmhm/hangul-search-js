593d67f4138228d9c5346a30fe7027c12e6b6f6a
hmmhmmhm <hmmhmmhm@naver.com>
2021-09-26 00:24:36 +0900

    feat: Update export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export declare const createSearch: (items: string[]) => (keyword: string, options?: {
     exact?: boolean | undefined;
+    order?: boolean | undefined;
 } | undefined) => string[];
 export default createSearch;
--- dist/index.js
+++ dist/index.js
@@ -16,16 +16,34 @@ var createSearch = function (items) {
         var founds = [];
         itemCounts.map(function (itemCount, index) {
             var matchCount = 0;
+            var exactFullCheck = (options === null || options === void 0 ? void 0 : options.exact) === true ? Object.keys(keywordCounts).length : 0;
+            var exactCheck = 0;
+            var exactOrderCheck = [];
             Object.entries(itemCount).map(function (_a) {
                 var itemKey = _a[0], itemValue = _a[1];
                 Object.entries(keywordCounts).map(function (_a) {
                     var keywordKey = _a[0], keywordValue = _a[1];
-                    if (itemKey === keywordKey && itemValue >= keywordValue)
+                    if (itemKey === keywordKey && itemValue >= keywordValue) {
                         matchCount += keywordValue;
+                        if ((options === null || options === void 0 ? void 0 : options.exact) === true)
+                            exactCheck += 1;
+                        if ((options === null || options === void 0 ? void 0 : options.order) === true)
+                            exactOrderCheck.push(itemKey);
+                    }
                 });
             });
-            if (matchCount !== 0)
-                founds.push([items[index], matchCount]);
+            if ((options === null || options === void 0 ? void 0 : options.order) === true) {
+                if (!Object.keys(keywordCounts).every(function (element, index) { return element === exactOrderCheck[index]; }))
+                    return;
+            }
+            if ((options === null || options === void 0 ? void 0 : options.exact) === true) {
+                if (matchCount !== 0 && exactFullCheck === exactCheck)
+                    founds.push([items[index], matchCount]);
+            }
+            else {
+                if (matchCount !== 0)
+                    founds.push([items[index], matchCount]);
+            }
         });
         return founds.sort(function (min, max) { return max[1] - min[1]; }).map(function (found) { return found[0]; });
     };
